93d2770f2f2f5388b75355f722b8e91905042c21
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-11 16:58:28 -0400

    save save save

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,36 +4,40 @@ var purp = 0;
 var green = 0;
 var blue = 0;
 var red = 0;
-var score = 0;
+var count = 0;
 
 function getRandom(max, min) {
     return [Math.floor(Math.random() * (max - min) + min)];
 }
 
 function assignRupeeValues() {
-    purp = getRandom(12, 1);
+    purp = getRandom(13, 1);
     console.log(purp);
-    green = getRandom(12, 1);
+    green = getRandom(13, 1);
     console.log(green);
-    red = getRandom(12, 1);
+    red = getRandom(13, 1);
     console.log(red);
-    blue = getRandom(12, 1);
+    blue = getRandom(13, 1);
     console.log(blue);
+}
+
+function assignValueAttr () {
+    $("#purp").attr("value", purp);
 }
 
 function startGame() {
     var randomNumber = getRandom(120, 19);
-    console.log(randomNumber);
+    $("#randomNumber").text("Random Number: " + randomNumber);
     assignRupeeValues();
-    $("purp").attr("value", purp);
-}
+    console.log(purp, red, green, blue);
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
+    assignValueAttr();
 
-$("button").on("click", adder());
-
-
-function adder() {
+    
     
 }
+
 
 startGame();
 
